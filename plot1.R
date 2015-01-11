7b83a69b26187f107cfd8309142c65f99ef638d8
pwvirgo <pwvirgo@gmail.com>
2015-01-11 02:45:35 -0500

    Fix plot1 code added plot2.png

--- plot1.R
+++ plot1.R
@@ -7,6 +7,28 @@ setwd("~/a/highEd/dataScience_coursera/ExploreData/data")
 hpc=read.table("household_power_consumption.txt", sep=";", header=TRUE, 
 	nrows=69518)
 hpc$Date<-as.Date(hpc$Date, format="%d/%m/%Y")
-hpc<-hpc[hpc$Date >="2007-02-01" && hpc$Date<="2007-02-02",]
+hpc<-hpc[hpc$Date >="2007-02-01" & hpc$Date<="2007-02-02",]
+
+hpc$Global_active_power<-as.character(hpc$Global_active_power)
+hpc$Global_active_power<-as.numeric(hpc$Global_active_power)
+
+png("plot1.png", width=480, height=480, res=120)
+with(hpc, hist(Global_active_power, main="Global Active Power",
+			   xlab="Global Active Power (kilowatts)", col="red"))
+dev.off()
+
+
+
+# hist(hpc$Global_active_power, main="Global Active Power",
+#	 xlab="Global Active Power (kilowatts)", col="red")
+
+# with(hpc, hist(Global_active_power, main="Global Active Power",
+# 	 xlab="Global Active Power (kilowatts)", col="red"))
+# 
+# pdf(file="plt.pdf")
+# with(hpc, hist(Global_active_power, main="Global Active Power",
+# 			   xlab="Global Active Power (kilowatts)", col="red"))
+# 
+# dev.off()
 
 
